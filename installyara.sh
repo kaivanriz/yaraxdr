a779b68c448186992623a66b3db8983ebd956e7c
Muhammad Ridwan Na'im <72285449+noplanalderson@users.noreply.github.com>
2025-03-10 15:14:25 +0700

    Update installyara.sh

--- installyara.sh
+++ installyara.sh
@@ -7,22 +7,36 @@
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
 
-#
-#------------------------- Aadjust IFS to read files -------------------------#
+# Adjust IFS to read files
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
+
 # Static active response parameters
-LOCAL=`dirname $0`
-#------------------------- Folder where Yara rules (files) will be placed -------------------------#
-mkdir /tmp/quarantined
+LOCAL=$(dirname "$0")
+
+# Detect OS type
+OS_TYPE=$(uname -s)
+OS_FAMILY="unknown"
+if [[ -f /etc/os-release ]]; then
+    . /etc/os-release
+    OS_FAMILY=$ID
+elif [[ "$OS_TYPE" == "Darwin" ]]; then
+    OS_FAMILY="macos"
+elif [[ "$OS_TYPE" == "Linux" ]]; then
+    OS_FAMILY="linux"
+fi
+
+# Create quarantine folder
+mkdir -p /tmp/quarantined
+
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-install_yara() {
+install_yara_from_source() {
   wget -N https://github.com/VirusTotal/yara/archive/refs/tags/v4.5.1.tar.gz
   tar -zxf v4.5.1.tar.gz
-  cd yara-4.5.1
+  cd yara-4.5.1 || exit 1
   ./bootstrap.sh
   ./configure
   make
@@ -31,81 +45,79 @@ install_yara() {
   mv yara /usr/bin/
   mv yarac /usr/bin/
 }
-install_yara2() {
-    yum install -y yara 
+
+install_yara_linux() {
+    if command_exists dnf; then
+        sudo dnf install -y yara
+    else
+        sudo yum install -y yara
+    fi
 }
 
+install_yara_macos() {
+    brew install yara
+}
 
+# Install Yara based on OS
 if command_exists yara; then
-  echo "Yara is already installed."
-
+    echo "Yara is already installed."
 else
-  install_yara2
-  echo "Yara installation script completed."
-
+    case "$OS_FAMILY" in
+        "ubuntu"|"debian") sudo apt update && sudo apt install -y yara ;; 
+        "centos"|"rhel"|"fedora") install_yara_linux ;;
+        "macos") install_yara_macos ;;
+        *) install_yara_from_source ;;
+    esac
+    echo "Yara installation completed."
 fi
 
+# Git repository setup
 git_repo_folder="/usr/local/signature-base"
-if [ ! -d "$git_repo_folder" ]; then
-    # If the directory doesn't exist, create it
-    mkdir -p "$git_repo_folder"
-    echo "Directory $git_repo_folder created."
-else
-    echo "Directory $git_repo_folder already exists."
+mkdir -p "$git_repo_folder"
+
+if [ ! -d "$git_repo_folder/.git" ]; then
+    cd "$git_repo_folder" || exit 1
+    git init
+    echo "Initialized Git repository in $git_repo_folder"
 fi
 
-if [ -d "$git_repo_folder/.git" ]; then
-        echo "Directory $git_repo_folder is a Git repository."
-else
-        echo "Directory $git_repo_folder is not a Git repository. Initializing..."
-        cd "$git_repo_folder"
-        git init
-        echo "Directory $git_repo_folder initialized as a Git repository."
-fi
-
-
-yara_file_extenstions=( ".yar" )
-yara_rules_list="/usr/local/signature-base/yara_rules_list.yar"
-
-#------------------------- Main workflow --------------------------#
-
-# Update Github Repo
-cd $git_repo_folder
-
-
+# Update GitHub Repo
+cd "$git_repo_folder" || exit 1
 git pull https://github.com/noplanalderson/yara_rules.git
 
-# Remove .yar files not compatible with standard Yara package
-rm -f $git_repo_folder/yara/gen_fake_amsi_dll.yar
-rm -f $git_repo_folder/yara/gen_vcruntime140_dll_sideloading.yar
-rm -f $git_repo_folder/yara/expl_connectwise_screenconnect_vuln_feb24.yar
-rm -f $git_repo_folder/yara/yara-rules_vuln_drivers_strict_renamed.yar
-rm -f $git_repo_folder/yara/gen_mal_3cx_compromise_mar23.yar
-rm -f $git_repo_folder/yara/expl_citrix_netscaler_adc_exploitation_cve_2023_3519.yar
-rm -f $git_repo_folder/yara/yara_mixed_ext_vars.yar
-rm -f $git_repo_folder/yara/thor_inverse_matches.yar
-rm -f $git_repo_folder/yara/generic_anomalies.yar
-rm -f $git_repo_folder/yara/gen_webshells_ext_vars.yar
-rm -f $git_repo_folder/yara/general_cloaking.yar
-rm -f $git_repo_folder/yara/configured_vulns_ext_vars.yar
-rm -f $git_repo_folder/yara/php_webshell_rules.yara
+# Remove incompatible Yara files
+declare -a yara_blacklist=(
+    "gen_fake_amsi_dll.yar"
+    "gen_vcruntime140_dll_sideloading.yar"
+    "expl_connectwise_screenconnect_vuln_feb24.yar"
+    "yara-rules_vuln_drivers_strict_renamed.yar"
+    "gen_mal_3cx_compromise_mar23.yar"
+    "expl_citrix_netscaler_adc_exploitation_cve_2023_3519.yar"
+    "yara_mixed_ext_vars.yar"
+    "thor_inverse_matches.yar"
+    "generic_anomalies.yar"
+    "gen_webshells_ext_vars.yar"
+    "general_cloaking.yar"
+    "configured_vulns_ext_vars.yar"
+    "php_webshell_rules.yara"
+)
 
-wget -P $git_repo_folder/yara/ https://raw.githubusercontent.com/CpanelInc/tech-CSI/master/php_webshell_rules.yara
-
-
-# Create File with rules to be compiled
-if [ ! -f $yara_rules_list ]
-then
-    /usr/bin/touch $yara_rules_list
-else rm $yara_rules_list
-fi
-for e in "${yara_file_extenstions[@]}"
-do
-  for f1 in $( find $git_repo_folder/yara -type f | grep -F $e ); do
-    echo "include \"""$f1"\""" >> $yara_rules_list
-  done
+for rule in "${yara_blacklist[@]}"; do
+    rm -f "$git_repo_folder/yara/$rule"
 done
-# Compile Yara Rules
-yarac $yara_rules_list /usr/local/signature-base/yara_base_ruleset_compiled.yar
+
+# Download replacement rule
+wget -P "$git_repo_folder/yara/" https://raw.githubusercontent.com/CpanelInc/tech-CSI/master/php_webshell_rules.yara
+
+# Create and compile Yara rules
+yara_rules_list="$git_repo_folder/yara_rules_list.yar"
+rm -f "$yara_rules_list"
+touch "$yara_rules_list"
+
+for f in "$git_repo_folder/yara"/*.yar; do
+    echo "include \"$f\"" >> "$yara_rules_list"
+done
+
+yarac "$yara_rules_list" "$git_repo_folder/yara_base_ruleset_compiled.yar"
 IFS=$SAVEIFS
-exit 1;
+exit 0
